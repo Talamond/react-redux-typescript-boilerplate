a4333fe2607596868b54fca7c8a76e7ff56c5de5
Jonathan Sweetman <truesweetman@gmail.com>
2021-02-10 19:59:52 -0500

    fix images

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -1,8 +1,6 @@
 import { navigate } from 'app/utils/navHelper';
 import React from 'react';
 require('./header.css');
-// import Github from 'svg-react-loader!../../../assets/images/name.svg'
-// import Icon from '../../../assets/images/github.svg';
 const Github = require('../../../assets/images/github.svg');
 
 export interface Props {
@@ -17,11 +15,9 @@ export const Header = (props: Props): JSX.Element => {
   return (
     <header className="header">
       {props.headers.map((header) => (<div className="header__elem" key={header.label} onClick={(e: React.MouseEvent<HTMLElement>) => navigate(e, header.path)}>{header.label}</div>))}
-      <div className="header__githubContainer">
-        <a href="https://github.com/Talamond">
-          <Github />
-        </a>
-      </div>
+      <a className="header__githubLink" href="https://github.com/Talamond">
+        <Github />
+      </a>
     </header>
   );
 };
