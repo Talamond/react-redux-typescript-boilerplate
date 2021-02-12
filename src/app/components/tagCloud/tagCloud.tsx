acf9df4ac8d9b6334eb63c1ac9b614de09445d45
Jonathan Sweetman <truesweetman@gmail.com>
2021-02-11 21:14:27 -0500

    new tag cloud

--- src/app/components/tagCloud/tagCloud.tsx
+++ src/app/components/tagCloud/tagCloud.tsx
@@ -35,13 +35,27 @@ const createWords = (props: Props): WordElem[] => {
   return elems;
 };
 
+const colors = ['#1b82d1', '#efb911', '#d83456', '#e97125', '#04a699', '#b521bd', '#21c6f3', '#906d4d', '#f990e2', '#8346b4', '#6d349b', '#582383'];
+
 const options = {
   fontFamily: 'HelveticaNeue-CondensedBlack',
   fontWeight: 'bold',
+  fontSizes: [20,70],
   enableTooltip: false,
   padding: 0,
   rotations: 1,
   rotationAngles: [0]
+};
+
+let colorIndex = -1;
+const callbacks = {
+  getWordColor: (word: any) => {
+    colorIndex++;
+    if (colorIndex >= colors.length) {
+      colorIndex = 0;
+    }
+    return colors[colorIndex];
+  }
 };
 
 export const TagCloud = (props: Props) => {
@@ -53,6 +67,6 @@ export const TagCloud = (props: Props) => {
   }, [data, height, factor]);
 
   return <div className="tagCloud" key={`tagcloud-${id}`}>
-    <ReactWordcloud words={wordElems} options={options} />
+    <ReactWordcloud words={wordElems} options={options} callbacks={callbacks}/>
   </div>;
 };
